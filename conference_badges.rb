8f9a41e57dcde7e6a43843bcec6ef034c6c1b86e
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-14 01:45:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+"Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  results = []
+ speakers.each do |speaker|
+  results << "Hello, my name is #{speaker}."
+  end
+  results
+ end
+ 
+def assigns_rooms(peoples)
+  rooms = []
+ peoples.each_with_index do |people, idx|
+   rooms << "Hello, #{attendee}! You'll be assigned to room #{idx}!"
+  end
+  rooms
+  end
+  
+  def printer
+  puts batch_badge_creator
+  puts assign_rooms
+end
